a38f346b357202fe388d484491bbc9336023b49c
Jason Torres <jtorr124@ucsc.edu>
2024-01-28 16:58:20 -0800

    adding mods

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+// Name: Jason Torres
+// Rocket Patrol Remastered
+// about 10 Hours
+// Mods List:
+// Track a high score that persists across scenes and display it in the UI (1)
+// Implement the speed increase that happens after 30 seconds in the original game(1)
+// Display the time remaining (in seconds) on the screen (3)
+// Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves, faster and is worth more points (5)
+// Implement a new timing/scoring mechanism that adds time to the clock for succesful hits (5)
+// Implement mouse control for player movement and left mouse click to fire (5)
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
